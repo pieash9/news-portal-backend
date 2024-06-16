d9746a4c38898e2d32c902992fea8935afeb29dd
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2024-06-16 22:52:41 +0600

    added user login with token generate

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,8 @@
 import vine, { errors } from "@vinejs/vine";
-import { registerSchema } from "../validations/authValidation.js";
+import { loginSchema, registerSchema } from "../validations/authValidation.js";
 import bcrypt from "bcryptjs";
 import prisma from "../DB/db.config.js";
+import jwt from "jsonwebtoken";
 
 class AuthController {
   static async register(req, res) {
@@ -46,7 +47,55 @@ class AuthController {
     }
   }
 
-  static async login(req, res) {}
+  static async login(req, res) {
+    try {
+      const body = req.body;
+      const validator = vine.compile(loginSchema);
+      const payload = await validator.validate(body);
+
+      //   find  user with email
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        if (!bcrypt.compareSync(payload.password, findUser.password)) {
+          return res.status(400).json({ message: "Wrong email or password" });
+        }
+        // issue token to user
+        const payloadData = {
+          id: findUser.id,
+          name: findUser.name,
+          email: findUser.email,
+          profile: findUser.profile,
+        };
+
+        const token = jwt.sign(payloadData, process.env.JWT_SECRET, {
+          expiresIn: "365d",
+        });
+
+        return res.json({
+          message: "Logged in",
+          access_token: `Bearer ${token}`,
+        });
+      }
+
+      return res.status(400).json({ message: "No user found with this email" });
+    } catch (error) {
+      console.log({ error });
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);
+        return res.status(400).json({ errors: error.messages });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong. Please try again!",
+        });
+      }
+    }
+  }
 }
 
 export default AuthController;
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ import AuthController from "../controllers/AuthController.js";
 const rootRoutes = Router();
 
 rootRoutes.post("/auth/register", AuthController.register);
+rootRoutes.post("/auth/login", AuthController.login);
 
 export default rootRoutes;
